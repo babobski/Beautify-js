2c1b2e279adc916637f11fd56ce9432de9abcb07
babobski <info@babobski.nl>
2016-05-18 21:40:44 +0200

    updated plugin to work with komodo 10, updated scripts, added IDE menu under code > format

--- defaults/preferences/beautifyjs-prefs.js
+++ defaults/preferences/beautifyjs-prefs.js
@@ -1,4 +1,4 @@
-pref("extensions.beautifyjs.eol", '\n');
+pref("extensions.beautifyjs.eol", '\\n');
 pref("extensions.beautifyjs.indent", 1);
 pref("extensions.beautifyjs.maxPreserveNewlines", 5);
 pref("extensions.beautifyjs.wrapLineLength", 0);
